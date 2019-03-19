24ab47aa17da1b1523ff111b6fe88b5c94446a97
hui <1179757089@qq.cm>
2019-03-19 21:53:02 +0800

    计算hash值

---
+++ algorithmcollection/src/demoone/Recruitment.java
@@ -0,0 +1,18 @@
+package demoone;
+
+import java.util.Scanner;
+
+public class Recruitment {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		
+		System.out.println(str.hashCode());
+
+	}
+
+}
